3ba6ebc4852eabf7919ff0790e3d7f30e12ddfad
EELMMAD <elmiramadadi@gmail.com>
2022-07-07 19:14:42 +0200

    Initial commit

---
+++ src/main/java/se/oracel/elmira/objectwiseTests/loopConstructors/SimpleLoop.java
@@ -0,0 +1,14 @@
+package se.oracel.elmira.objectwiseTests.loopConstructors;
+
+public class SimpleLoop {
+    public static void main(String[] args) {
+        int i=0, j=10;
+        int count = 0;
+        while (i<j) {
+            i++;
+            j--;
+            count++;
+        }
+        System.out.println(i+" "+j+" "+count);
+    }
+}
