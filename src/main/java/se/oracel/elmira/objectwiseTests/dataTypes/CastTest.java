4aa0cac60227415d1ca7018e5e04b418e857dbbd
EELMMAD <elmiramadadi@gmail.com>
2022-07-09 11:48:56 +0200

    Initial commit

---
+++ src/main/java/se/oracel/elmira/objectwiseTests/dataTypes/CastTest.java
@@ -0,0 +1,10 @@
+package se.oracel.elmira.objectwiseTests.dataTypes;
+
+public class CastTest {
+    public static void main(String args[ ] ){
+        byte b = -128 ;
+        int i = b ;
+        b = (byte) i;
+        System.out.println(i+" "+b);
+    }
+}
