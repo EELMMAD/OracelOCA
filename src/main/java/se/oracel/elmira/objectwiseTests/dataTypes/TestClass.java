42b6884b9c6c877720621b8a672526ba50f27f83
EELMMAD <elmiramadadi@gmail.com>
2022-07-09 12:11:37 +0200

    Initial commit

---
+++ src/main/java/se/oracel/elmira/objectwiseTests/dataTypes/TestClass.java
@@ -0,0 +1,16 @@
+package se.oracel.elmira.objectwiseTests.dataTypes;
+
+public class TestClass {
+    public static Integer wiggler(Integer x){
+        Integer y = x + 10;
+        x++;
+        System.out.println(x);
+        return y;
+    }
+
+    public static void main(String[] args){
+        Integer dataWrapper = new Integer(5);
+        Integer value = wiggler(dataWrapper);
+        System.out.println(dataWrapper+value);
+    }
+}
