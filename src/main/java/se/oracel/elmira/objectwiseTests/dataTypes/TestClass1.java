01c8406eab42833e3266fcf0706d1c0724b5a489
EELMMAD <elmiramadadi@gmail.com>
2022-07-09 15:10:50 +0200

    Initial commit

---
+++ src/main/java/se/oracel/elmira/objectwiseTests/dataTypes/TestClass1.java
@@ -0,0 +1,14 @@
+package se.oracel.elmira.objectwiseTests.dataTypes;
+
+public class TestClass1 {
+    public static void main(String[] args) {
+
+        int x = 1____3;   //1
+
+        long y = 1_3;     //2
+
+        float z = 3.234_567f; //3
+
+        System.out.println(x+" "+y+" "+z);
+    }
+}
