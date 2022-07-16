92d7a7bd70f8d41ede94d5c48c40f3df4213dd15
EELMMAD <elmiramadadi@gmail.com>
2022-07-16 08:36:37 +0200

    Initial commit

---
+++ src/main/java/se/oracel/elmira/objectwiseTests/api/Test4.java
@@ -0,0 +1,17 @@
+package se.oracel.elmira.objectwiseTests.api;
+
+public class Test4 {
+    public static void stringTest(String s){
+        s.replace('h', 's');
+    }
+    public static void stringBuilderTest(StringBuilder s){
+        s.append("o");
+    }
+    public static void main(String[] args){
+        String s = "hell";
+        StringBuilder sb = new StringBuilder("well");
+        stringTest(s);
+        stringBuilderTest(sb);
+        System.out.println(s + sb);
+    }
+}
