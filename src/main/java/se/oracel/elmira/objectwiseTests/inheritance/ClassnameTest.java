403f43a0daf49753244a93040a3184a2db1d1570
EELMMAD <elmiramadadi@gmail.com>
2022-07-20 10:55:42 +0200

    Initial commit

---
+++ src/main/java/se/oracel/elmira/objectwiseTests/inheritance/ClassnameTest.java
@@ -0,0 +1,15 @@
+package se.oracel.elmira.objectwiseTests.inheritance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClassnameTest {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder("mrx");
+        String s = sb.toString();
+        list.add(s);
+        System.out.println(s.getClass());
+        System.out.println(list.getClass());
+}
+}
