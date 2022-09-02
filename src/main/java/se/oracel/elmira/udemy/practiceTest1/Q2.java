809ec64a7fde922a44171dc3d79ad6fbfa94eadf
EELMMAD <elmiramadadi@gmail.com>
2022-09-02 09:50:12 +0200

    Initial commit

---
+++ src/main/java/se/oracel/elmira/udemy/practiceTest1/Q2.java
@@ -0,0 +1,12 @@
+package se.oracel.elmira.udemy.practiceTest1;
+
+public class Q2 {
+    static {
+        System.out.println(1/0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+    }
+
+}
