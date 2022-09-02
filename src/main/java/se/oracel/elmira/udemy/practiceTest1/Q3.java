73cfa8cad425d3fde9c0cf8d6c52e61ba0df2df0
EELMMAD <elmiramadadi@gmail.com>
2022-09-02 09:56:36 +0200

    Initial commit

---
+++ src/main/java/se/oracel/elmira/udemy/practiceTest1/Q3.java
@@ -0,0 +1,11 @@
+package se.oracel.elmira.udemy.practiceTest1;
+
+public class Q3 {
+    public static void main(String[] args) {
+        String s1 = "OCAJP";
+        String s2 = "OCAJP" + "";
+        System.out.println(s1 == s2);
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
