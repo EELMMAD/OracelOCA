60c7aa96102c4c266fbbb9234e7e2be121a57296
EELMMAD <elmiramadadi@gmail.com>
2022-09-10 21:21:49 +0200

    Initial commit

---
+++ src/main/java/se/oracel/elmira/udemy/practiceTest1/Q25.java
@@ -0,0 +1,10 @@
+package se.oracel.elmira.udemy.practiceTest1;
+
+public class Q25 {
+    public static void main(String[] args) {
+        int [] arr = {2, 1, 0};
+        for(int i : arr) {
+            System.out.println(arr[i]);
+        }
+    }
+}
