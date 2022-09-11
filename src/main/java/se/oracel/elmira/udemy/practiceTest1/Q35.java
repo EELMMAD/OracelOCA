c27c70a81257d5496c92dabf42d2a4027bb0d762
EELMMAD <elmiramadadi@gmail.com>
2022-09-11 23:01:40 +0200

    Initial commit

---
+++ src/main/java/se/oracel/elmira/udemy/practiceTest1/Q35.java
@@ -0,0 +1,12 @@
+package se.oracel.elmira.udemy.practiceTest1;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+public class Q35 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder(100);
+        System.out.println(sb.length() + ":" + sb.toString().length());
+    }
+}
