ce70f88ed9b124dfe21740770aef85f850c153d1
EELMMAD <elmiramadadi@gmail.com>
2022-11-30 17:36:02 +0100

    Initial commit

---
+++ src/main/java/se/oracel/elmira/practic/easy/Q6.java
@@ -0,0 +1,12 @@
+package se.oracel.elmira.practic.easy;
+
+public class Q6 {
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 1;
+        if( (i++ == 0) & (j++ == 2) ){
+            i = 12;
+        }
+        System.out.println(i+" "+j);
+    }
+}
