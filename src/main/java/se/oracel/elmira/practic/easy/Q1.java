93d7aafae63e3e9ea42954562ef8bcb5106400c3
EELMMAD <elmiramadadi@gmail.com>
2022-11-30 12:45:40 +0100

    Initial commit

---
+++ src/main/java/se/oracel/elmira/practic/easy/Q1.java
@@ -0,0 +1,10 @@
+package se.oracel.elmira.practic.easy;
+
+public class Q1 {
+    public static void main(String[] args) {
+        int index = 1;
+        String[] strArr = new String[5];
+        String   myStr  = strArr[index];
+        System.out.println(myStr);
+    }
+}
